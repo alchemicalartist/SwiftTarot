553cd430d3e19e1da88870532ff9637bb7920fd8
Chris Denton <chris@intuitivealchemy.studio>
2023-01-07 03:52:23 -0800

    huh

--- Sources/SwiftTarot/Deck.swift
+++ Sources/SwiftTarot/Deck.swift
@@ -6,85 +6,90 @@ extension SwiftTarot {
     public struct Deck:  Equatable, CustomStringConvertible, Sequence, Collection {
         public typealias DeckIndex = Int
         public typealias Index = Int
-        public typealias Element = TarotCard
-        public typealias Cards = [TarotCard]
-        public typealias DeckIndexRange = Range<DeckIndex>
-        public typealias DeckSlice = ArraySlice<TarotCard>
+        public typealias Element = SwiftTarot.TarotCard
+        public typealias Cards = [SwiftTarot.TarotCard]
+        public typealias DeckIndexRange = Range<Index>
+        public typealias DeckSlice = ArraySlice<Element>
         public typealias Iterator = SwiftTarot.Deck.Cards.Iterator
         fileprivate var cards: Cards = []
         public let deckSize = 78
-        public init(_ cards: [TarotCard]) {
+        public init(_ cards: [Element]) {
             self.cards = cards
         }
-        public init(arrayLiteral elements: TarotCard...) {
+        public init(arrayLiteral elements: Element...) {
             self.init(elements)
         }
-        public subscript(position: DeckIndex) -> TarotCard {
+        public subscript(position: Index) -> Element {
             precondition(cards.indices.contains(position), "out of bounds")
-            return cards[position]
+            return self.cards[position]
         }
-        public subscript(bounds: Range<DeckIndex>) -> DeckSlice {
-            cards[bounds]
+        public subscript(bounds: Range<Index>) -> ArraySlice<Element> {
+            self.cards[bounds]
         }        
-        private mutating func cutDeckAt(_ i: Int) {
+        private mutating func cutDeckAt(_ i: Index) {
             var new: [TarotCard] = []
             new.append(contentsOf: cards[i...cards.endIndex])
             new.append(contentsOf: cards[cards.startIndex..<i])
             cards = new
         }
-        public mutating func faceUp(at idx: Int) {
+        public mutating func faceUp(at idx: Index) {
             var card = cards[idx]
             cards.remove(at: idx)
             card.faceUp = true
             cards.insert(card, at: idx)
         }
         public mutating func shuffle() {
-            var shuffled = cards.shuffled()
+            var shuffled = self.cards.shuffled()
+            var g: [Int] = []
             (0..<(Int.random(in: 0..<deckSize) % 7)).forEach { _ in
-                var revCard = cards.randomElement()!
-                let index = shuffled.firstIndex(of: revCard)!
+                let r = Int.random(in: 0..<deckSize)
+                while g.contains(r) == true {
+                    r = Int.random(in: 0..<deckSize)
+                }
+                g.append(r)
+                var revCard = cards[r]
                 revCard.reversed.toggle()
-                shuffled.replaceSubrange(index...index, with: [revCard])
+                shuffled.replaceSubrange(r...r, with: [revCard])
             }
-            cards = shuffled
+            self.cards = shuffled
         }
         public var count: Int {
-            cards.count
+            self.cards.count
         }
         internal func randomCard() -> TarotCard {
-            cards.randomElement()!
+            self.cards.randomElement()!
         }
-        fileprivate mutating func insert(_ c: TarotCard, at i: Int) {
-            cards.insert(c, at: i)
+        fileprivate mutating func insert(_ c: Element, at i: Index) {
+            self.cards.insert(c, at: i)
         }
-        fileprivate mutating func remove(at i: Int) {
-            cards.remove(at: i)
+        fileprivate mutating func remove(at i: Index) {
+            self.cards.remove(at: i)
         }
-        fileprivate mutating func append(_ c: TarotCard) {
-            cards.append(c)
+        fileprivate mutating func append(_ c: Element) {
+            self.cards.append(c)
         }
-        public mutating func lastIndex(where p: @escaping ((TarotCard) -> Bool)) -> Int {
-            cards.lastIndex(where: p)!
+        public mutating func lastIndex(where p: @escaping ((Element) -> Bool)) -> Index {
+            self.cards.lastIndex(where: @escaping p)!
         }
         public var description: String {
             var res = ""
-            cards.forEach { card in
+            self.cards.forEach { card in
                 res += String(describing: card) + "\n"
             }
             return res
         }
         public func makeIterator() -> Iterator {
-            Iterator(_elements: cards)
+            self.cards.makeIterator()
         }
-        public var startIndex: Int {
-            cards.startIndex
+        public var startIndex: Index {
+            self.cards.startIndex
         }
-        public var endIndex: Int {
-            cards.endIndex
+        public var endIndex: Index {
+            self.cards.endIndex
         }
         
-        public func index(after i: Int) -> Int {
-            return cards.index(after: i)
+        public func index(after i: Index) -> Index {
+            return self.cards.index(after: i)
         }
         
     }
